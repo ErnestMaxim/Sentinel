7c519edb50195ba14dfcce93e7d5b05bd7684a0d
ErnestGMaxim <ernest.maxim@student.unitbv.ro>
2026-06-20 22:57:58 +0300

    (fix): Changed what files to be downloaded

--- ml-service/artifacts_manager.py
+++ ml-service/artifacts_manager.py
@@ -65,12 +65,18 @@ def ensure_artifacts(
         )
 
     snapshot_download(
-        repo_id=hf_repo_id,
-        repo_type=repo_type,
-        local_dir=str(artifacts_dir),
-        token=hf_token,
-        ignore_patterns=[".gitattributes", "*.git*"],
-        local_dir_use_symlinks=False,
+    repo_id=hf_repo_id,
+    repo_type=repo_type,
+    local_dir=str(artifacts_dir),
+    token=hf_token,
+    ignore_patterns=[
+        ".gitattributes", "*.git*",
+        "faiss_document_index.bin",
+        "faiss_metadata.pkl",
+        "faiss_texts.pkl",
+        "category_indexes/*",
+    ],
+    local_dir_use_symlinks=False,
     )
 
     # Verify after download
